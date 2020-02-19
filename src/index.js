31d7572a3c3e9d5ad8bb074c90d49703df866ff0
PavelSashnikov <44900422+PavelSashnikov@users.noreply.github.com>
2020-02-19 20:34:31 +0300

    feat: task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  const numArr = String(n).split('');
+  if(numArr[0] === '-') numArr.shift();
+  const reversedStr = numArr.reverse().join('');
+  return +reversedStr;
 }
